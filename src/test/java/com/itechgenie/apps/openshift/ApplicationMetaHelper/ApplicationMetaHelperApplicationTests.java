9108f2f6f3055cdc1cb609fe34e8fc144e4e9ada
Prakash <prakashm88@yahoo.co.in>
2024-08-15 19:12:32 +0530

    my commit

---
+++ src/test/java/com/itechgenie/apps/openshift/ApplicationMetaHelper/ApplicationMetaHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itechgenie.apps.openshift.ApplicationMetaHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationMetaHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
